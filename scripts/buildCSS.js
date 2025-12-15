a4e0a3ea54fdaf476fe2ab24aef17c6cda37ea45
Russell S. Lebo <russell@dataink.io>
2025-12-15 17:46:05 -0500

    Installing @datainkio packages on laptop

--- scripts/buildCSS.js
+++ scripts/buildCSS.js
@@ -39,7 +39,7 @@ import { TailwindLogger } from '../eleventy/services/TailwindLogger.js';
 import { execSync, spawn } from 'child_process';
 import { resolve, dirname } from 'path';
 import { fileURLToPath } from 'url';
-import logger, { LumberjackStyle } from '@datainkio/lumberjack';
+import logger, { LumberjackStyle } from '/assets/js/utils/lumberjack/index.js';
 
 // Enable logger for build transparency
 logger.enabled = true;
