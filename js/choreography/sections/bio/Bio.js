c140abe39e3c07007655d6b799190f440b4848ff
Russell S. Lebo <russell@dataink.io>
2026-05-12 12:35:05 -0400

    feat: refactor print marks implementation away from reliance on JS for generating visual elements towards a simpler, more modular, and less brittle approach using divs, svgs, and includes.

--- js/choreography/sections/bio/Bio.js
+++ js/choreography/sections/bio/Bio.js
@@ -26,7 +26,7 @@ import AbstractSection from "../abstract-section/AbstractSection.js";
 import { SELECTORS } from "../../config/index.js";
 import { BIO_SELECTORS } from "../../config/contracts/selectors.js";
 // import LineManager from "../../managers/LineManager.js";
-import { add as addPrinterMarks } from "../../../displays/PrinterMarks.js";
+// import { add as addPrinterMarks } from "../../../displays/PrinterMarks.js";
 import BioAnimations from "./BioAnimations.js";
 import BioTriggers from "./BioTriggers.js";
 
@@ -36,9 +36,10 @@ export default class Bio extends AbstractSection {
 
     const animations = new BioAnimations(view);
     const triggers = new BioTriggers(view);
-    if (view && !view.querySelector(".printmarks")) {
-      addPrinterMarks(view);
-    }
+    // JS-implementation of printer marks deprecated in favor of embedded SVGs.
+    // if (view && !view.querySelector(".printmarks")) {
+    //   addPrinterMarks(view);
+    // }
 
     super({
       view,
