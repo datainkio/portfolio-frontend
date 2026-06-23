d687cae5669f75a474a72d5a676f78b0d457976f
Russell S. Lebo <russell@dataink.io>
2026-06-23 11:28:31 -0400

    feat(organizations): display non-award organization logos

--- data/sanity/projections/organization/organizationProjection.js
+++ data/sanity/projections/organization/organizationProjection.js
@@ -1,11 +1,12 @@
 /** @format */
 import groq from "groq";
 /** Organization reference projection (inner shape only — excludes field name and traversal operator). */
+/** Used for rendering organization details in the frontend */
 export const ORGANIZATION_PROJECTION = groq`{
   _id,
+  _updatedAt,
   "title": page.title,
   "slug": page.slug.current,
-  description,
   "logo": logo->{
     "alt": image.alt,
     "asset": image.asset->{url, metadata{dimensions, lqip}}
