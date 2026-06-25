f6b7a7bbfd07b7469564e58049df6b35468993cc
Russell S. Lebo <russell@dataink.io>
2026-06-25 14:28:58 -0400

    chore: continue bringing organizations section into parity with other, more complete, sections.

--- data/sanity/queries/home.js
+++ data/sanity/queries/home.js
@@ -38,6 +38,8 @@ export const homeQuery = {
     },
     recognitionHeading,
     recognitionBody,
+    organizationsHeading,
+    organizationsBody,
     workHeading,
     workBody[]{
       ...,
--- js/choreography/organisms/organizations/OrganizationsAnimations.js
+++ js/choreography/organisms/organizations/OrganizationsAnimations.js
@@ -14,50 +14,50 @@ export default class OrganizationsAnimations extends AbstractSectionAnimations {
   constructor(view, options = {}) {
     super(view);
 
-    this.options = {
-      duration: options.duration ?? ORGANIZATIONS_ANIMATION_DEFAULTS.duration,
-      stagger: options.stagger ?? ORGANIZATIONS_ANIMATION_DEFAULTS.stagger,
-      translateY:
-        options.translateY ?? ORGANIZATIONS_ANIMATION_DEFAULTS.translateY,
-      itemTranslateY:
-        options.itemTranslateY ??
-        ORGANIZATIONS_ANIMATION_DEFAULTS.itemTranslateY,
-      itemRevealViewportRatio:
-        options.itemRevealViewportRatio ??
-        ORGANIZATIONS_ANIMATION_DEFAULTS.itemRevealViewportRatio,
-      ease: {
-        in: options.ease?.in ?? ORGANIZATIONS_ANIMATION_DEFAULTS.ease.in,
-        out: options.ease?.out ?? ORGANIZATIONS_ANIMATION_DEFAULTS.ease.out,
-      },
-    };
+    // this.options = {
+    //   duration: options.duration ?? ORGANIZATIONS_ANIMATION_DEFAULTS.duration,
+    //   stagger: options.stagger ?? ORGANIZATIONS_ANIMATION_DEFAULTS.stagger,
+    //   translateY:
+    //     options.translateY ?? ORGANIZATIONS_ANIMATION_DEFAULTS.translateY,
+    //   itemTranslateY:
+    //     options.itemTranslateY ??
+    //     ORGANIZATIONS_ANIMATION_DEFAULTS.itemTranslateY,
+    //   itemRevealViewportRatio:
+    //     options.itemRevealViewportRatio ??
+    //     ORGANIZATIONS_ANIMATION_DEFAULTS.itemRevealViewportRatio,
+    //   ease: {
+    //     in: options.ease?.in ?? ORGANIZATIONS_ANIMATION_DEFAULTS.ease.in,
+    //     out: options.ease?.out ?? ORGANIZATIONS_ANIMATION_DEFAULTS.ease.out,
+    //   },
+    // };
 
-    const elements = [
-      selectOrganizationsEl(this.view, "context"),
-      selectOrganizationsEl(this.view, "heading"),
-      selectOrganizationsEl(this.view, "body"),
-      selectOrganizationsEl(this.view, "list"),
-    ];
+    // const elements = [
+    //   selectOrganizationsEl(this.view, "context"),
+    //   selectOrganizationsEl(this.view, "heading"),
+    //   selectOrganizationsEl(this.view, "body"),
+    //   selectOrganizationsEl(this.view, "list"),
+    // ];
 
-    const organizationItems = Array.from(
-      this.view?.querySelectorAll(`[${ORGANIZATIONS_EL_ATTR}="organization"]`) ?? [],
-    );
+    // const organizationItems = Array.from(
+    //   this.view?.querySelectorAll(`[${ORGANIZATIONS_EL_ATTR}="organization"]`) ?? [],
+    // );
 
-    const { intro, outro } = createSectionHeaderIntro(elements, {
-      duration: this.options.duration,
-      stagger: this.options.stagger,
-      translateY: this.options.translateY,
-      ease: { in: this.options.ease.in, out: this.options.ease.out },
-    });
+    // const { intro, outro } = createSectionHeaderIntro(elements, {
+    //   duration: this.options.duration,
+    //   stagger: this.options.stagger,
+    //   translateY: this.options.translateY,
+    //   ease: { in: this.options.ease.in, out: this.options.ease.out },
+    // });
 
-    this._headerIntro = intro;
-    this._headerOutro = outro;
+    // this._headerIntro = intro;
+    // this._headerOutro = outro;
 
-    this.revealGroup = createScrollRevealGroup(organizationItems, {
-      viewportRatio: this.options.itemRevealViewportRatio,
-      duration: this.options.duration,
-      ease: this.options.ease.in,
-      y: this.options.itemTranslateY,
-    });
+    // this.revealGroup = createScrollRevealGroup(organizationItems, {
+    //   viewportRatio: this.options.itemRevealViewportRatio,
+    //   duration: this.options.duration,
+    //   ease: this.options.ease.in,
+    //   y: this.options.itemTranslateY,
+    // });
 
     this._buildTimeline();
   }
