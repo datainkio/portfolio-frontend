16dd65bf3e49aa618b8158761ab0ada16d392e62
Russell S. Lebo <russell@dataink.io>
2026-07-04 16:34:32 -0400

    feat: project page header

--- data/sanity/projections/project/projectPageProjection.js
+++ data/sanity/projections/project/projectPageProjection.js
@@ -1,5 +1,9 @@
 /** @format */
 import groq from "groq";
+import { ORGANIZATION_PROJECTION } from "../organization/organizationProjection.js";
+import { ROLE_PROJECTION } from "../role/roleProjection.js";
+import { ACTIVITY_PROJECTION } from "../activity/activityProjection.js";
+import { AWARD_PROJECTION } from "../award/awardProjection.js";
 /**
  * Project page projection (inner shape — excludes field name and traversal operator).
  * Used for project detail/page views with full relational data.
@@ -10,26 +14,13 @@ export const PROJECT_PAGE_PROJECTION = groq`{
   "title": page.title,
   "slug": page.slug.current,
   "abstract": page.abstract,
-  "industry": industry->{
-    _id,
-    "title": prefLabel,
-    conceptId
-  },
-  "organization": organization[]->{
-    _id,
-    "title": page.title,
-    "slug": page.slug.current,
-    "industry": industry->{
-      _id,
-      "preferredLabel": prefLabel
-    },
-    organizationType,
-    featured
-  },
-  "roles": roles->{_id, "title": prefLabel, conceptId},
-  "activities": activities->{_id, "title": prefLabel, conceptId},
-  "outcomes": outcomes->{_id, "title": prefLabel, conceptId},
-  "awards": awards->{_id, title, "slug": slug.current},
+  "industry": industry->prefLabel,
+  "rolesTitles": array::unique(roles[]->prefLabel),
+  "activityTitles": array::unique(activities[]->prefLabel),
+  "organization": organization[]->${ORGANIZATION_PROJECTION},
+  "roles": roles[]->${ROLE_PROJECTION},
+  "activities": activities[]->${ACTIVITY_PROJECTION},
+  "awards": awards[]->${AWARD_PROJECTION},
   body[]{
     ...,
     _type == "image" => {
