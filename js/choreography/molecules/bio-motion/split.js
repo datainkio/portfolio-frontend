6f9a4df8013ca3b6aef21cb86a974c6c02b232a4
Russell S. Lebo <russell@dataink.io>
2026-07-09 07:56:30 -0400

    feat: tune bio section sequencing

--- js/choreography/molecules/bio-motion/split.js
+++ js/choreography/molecules/bio-motion/split.js
@@ -68,8 +68,8 @@ export function intro(view, gelManager) {
     { duration: 0.5, opacity: 0, y: 100, rotation: 45, stagger: 0.015 },
     "-=0.3",
   );
-  tl.to(highlights, { color: tokenColor("secondary-600"), stagger: 0.85 });
-  tl.from(subheading, { duration: 0.5, opacity: 0, y: 100 }, "-=0.4");
+  tl.to(highlights, { color: tokenColor("secondary-600"), stagger: 1 });
+  tl.from(subheading, { duration: 0.5, opacity: 0, y: 100 });
   tl.from(
     aside?.children ?? [],
     { duration: 0.5, opacity: 0, y: 100, stagger: BIO_INTRO.stagger },
