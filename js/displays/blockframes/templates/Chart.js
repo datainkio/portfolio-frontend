144e9b7a8b916fd6b694108abf54644ff1bcba20
Russell S. Lebo <russell@dataink.io>
2026-07-24 13:50:01 -0400

    chore: remove console logs from blockframes

--- js/displays/blockframes/templates/Chart.js
+++ js/displays/blockframes/templates/Chart.js
@@ -18,7 +18,6 @@
 /** @format */
 
 export function paint(elem, palette) {
-  console.log("Chart", elem);
   const pie = elem.querySelector(".pie");
   const donut = elem.querySelector(".donut");
   const headlines = elem.querySelector(".headlines");
